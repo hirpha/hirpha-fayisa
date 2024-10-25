a341f5a705af888badf17b2c806bf54b974953b7
Hirpha Fayisa <hirphafayisa88@gmail.com>
2024-10-25 20:18:49 +0300

    upper photo changed

--- src/Pages/Home/Footer.jsx
+++ src/Pages/Home/Footer.jsx
@@ -146,7 +146,7 @@ function Footer() {
             </li>
             <li>
               <a
-                href="https://www.linkedin.com/"
+                href="https://www.linkedin.com/in/hirpha-fayisa-072051186/"
                 className="navbar--content"
                 target="_blank"
                 rel="noreferrer"
@@ -172,7 +172,7 @@ function Footer() {
       </div>
       <hr className="divider" />
       <div className="footer--content--container">
-        <p className="footer--content">Made with 💖 by John</p>
+        <p className="footer--content">Made with Hirpha Fayisa</p>
         <div className="footer--social--icon">
           <ul>
             <li>
--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -3,15 +3,16 @@ export default function HeroSection() {
     <section id="heroSection" className="hero--section">
       <div className="hero--section--content--box">
         <div className="hero--section--content">
-          <p className="section--title">Hey, I'm John</p>
+          <p className="section--title">Hey, I'm Hirpha</p>
           <h1 className="hero--section--title">
             <span className="hero--section-title--color">Full Stack</span>{" "}
             <br />
             Developer
           </h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            <br /> Dolorum, quas. Amet soluta assumenda cum?
+            I am a full-stack developer with expertise in both frontend and
+            backend, focused on creating responsive and high-quality
+            applications.
           </p>
         </div>
         <button className="btn btn-primary">Get In Touch</button>
